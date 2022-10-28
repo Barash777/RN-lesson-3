952ecefa20dcd19035c5af1e07adb70b05a24963
barash777 <barash.vertihvost@gmail.com>
2022-10-28 18:51:08 +0300

    feat: add UsersScreen

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -6,6 +6,16 @@ function HomeScreen() {
     return (
         <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
             <Text>Home Screen</Text>
+            <Text>Home Screen</Text>
+            <Text>Home Screen</Text>
+        </View>
+    );
+}
+
+function UsersScreen() {
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text>Users Screen</Text>
         </View>
     );
 }
@@ -17,6 +27,7 @@ export const Main = () => {
         <View style={styles.container}>
             <Stack.Navigator>
                 <Stack.Screen name={'Home'} component={HomeScreen}/>
+                <Stack.Screen name={'Users'} component={UsersScreen}/>
             </Stack.Navigator>
         </View>
     );
@@ -26,7 +37,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        // alignItems: 'center',
+        // justifyContent: 'center',
     },
 });
