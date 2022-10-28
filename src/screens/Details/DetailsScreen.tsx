34b8089bc088a433b5fbb494ac930c64d916357a
barash777 <barash.vertihvost@gmail.com>
2022-10-28 19:34:07 +0300

    feat: add props to DetailsScreen

--- src/screens/Details/DetailsScreen.tsx
+++ src/screens/Details/DetailsScreen.tsx
@@ -1,13 +1,17 @@
 import {Button, Text, View} from "react-native";
 import React from "react";
-import {useAppNavigation} from "../types";
+import {DetailsPropsType, useAppNavigation} from "../types";
 
-export function DetailsScreen() {
+export function DetailsScreen({route}: DetailsPropsType) {
     const navigation = useAppNavigation()
+    const {id, name, isDone} = route.params
 
     return (
         <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
             <Text>Details Screen</Text>
+            <Text>{id}</Text>
+            <Text>{name}</Text>
+            <Text>{isDone ? 'true' : 'false'}</Text>
             <Button title={'To Home'} onPress={() => {
                 navigation.navigate('Home')
             }}/>
